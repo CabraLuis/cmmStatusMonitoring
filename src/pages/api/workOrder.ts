4b3d61b1e7e0800bc9b707045e56261318c9afe7
Luis Cabra <zetinaluis03@gmail.com>
2025-01-30 09:28:21 -0600

    sorting

--- src/pages/api/workOrder.ts
+++ src/pages/api/workOrder.ts
@@ -81,6 +81,9 @@ export const GET: APIRoute = async () => {
       step: true,
       partStatusRegistry: true,
     },
+    orderBy: {
+      id: "desc",
+    },
   });
 
   return new Response(JSON.stringify(workOrders), {
